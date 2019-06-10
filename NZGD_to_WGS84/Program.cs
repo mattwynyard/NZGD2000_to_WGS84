8562707a63551d450330fdaec2b79696485c31b2
mattwynyard <mjwynyard@gmail.com>
2019-06-10 13:42:28 +1200

    intial commit

---
+++ NZGD_to_WGS84/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZGD_to_WGS84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
